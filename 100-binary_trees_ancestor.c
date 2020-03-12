105c2b95f8b29d3288cd9f8a2b499933d04701e5
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-12 20:44:15 +0000

    finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -58,7 +58,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		else
 		{
 			if (first == second)
-				return ((binary_tree_t*) first);
+				return ((binary_tree_t *) first);
 			first = first->parent;
 			second = second->parent;
 		}
