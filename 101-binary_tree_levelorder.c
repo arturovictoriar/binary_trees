6a55ec4378e4b2f946d849d8230ed4622003a2dc
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-13 00:29:53 +0000

    betty fixed at least

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -19,5 +19,4 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_levelorder(tree->left, func);
 		binary_tree_levelorder(tree->right, func);
 	}
-	return;
 }
