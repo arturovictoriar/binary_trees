0fabd2372fdb10772a4bfb90a3ad93241bb111f0
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-13 00:27:11 +0000

    fixed to use one function only

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,25 +1,6 @@
 #include "binary_trees.h"
 
 /**
-  * BT_levelorder - goes through a binary tree using level-order traversal
-  * @tree: root node of the BT
-  * @func: function to print the traverse
-  * Return: Nothing
-  */
-void BT_levelorder(const binary_tree_t *tree, void (*func)(int))
-{
-	if (!tree || !func)
-		return;
-
-	if (tree->left)
-		func(tree->left->n);
-	if (tree->right)
-		func(tree->right->n);
-	BT_levelorder(tree->left, func);
-	BT_levelorder(tree->right, func);
-}
-
-/**
   * binary_tree_levelorder - goes through abinarytreeusinglevel-ordertraversal
   * @tree: root node of the BT
   * @func: function to print the traverse
@@ -27,12 +8,16 @@ void BT_levelorder(const binary_tree_t *tree, void (*func)(int))
   */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
-
-	func(tree->n);
-	if (!tree->parent && !tree->left && !tree->right)
-		return;
-
-	BT_levelorder(tree, func);
+	if (tree && func)
+	{
+		if (!tree->parent)
+			func(tree->n);
+		if (tree->left)
+			func(tree->left->n);
+		if (tree->right)
+			func(tree->right->n);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+	return;
 }
